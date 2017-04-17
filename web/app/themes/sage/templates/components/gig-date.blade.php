e816be5fc5ee726e81583ed9852c4f7f2507a034
Scott Cox <scottcox89@gmail.com>
2017-04-17 12:19:52 +0100

    Adds partial and component for gig-date.

---
+++ web/app/themes/sage/templates/components/gig-date.blade.php
@@ -0,0 +1,1 @@
+<p>Gig Date</p>
---
+++ web/app/themes/sage/templates/partials/gig-dates.blade.php
@@ -0,0 +1,6 @@
+<section class="gig-dates">
+	<div class="container">
+		<h1 class="text-center">Upcoming Shows</h1>
+		@include('components.gig-date')
+	</div>
+</section>
