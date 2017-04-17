b2dcfbe369cf3716b0c2554059187dd9f72d9479
Scott Cox <scottcox89@gmail.com>
2017-04-17 12:22:13 +0100

    [templates] [pages] Updates page and front-page templates.

---
+++ web/app/themes/sage/templates/front-page.blade.php
@@ -0,0 +1,10 @@
+<!-- START: front-page.blade.php-->
+@extends('layouts.base')
+
+@section('content')
+	@include('partials.page-header')
+	@include('partials.featured-video')
+	@include('partials.gig-dates')
+	@include('partials.recent-photos')
+@endsection
+<!-- END: front-page.blade.php -->
