accd5a7d69bc5b5833de876d7b033e09b24002db
Scott Cox <scottcox89@gmail.com>
2017-04-19 21:48:11 +0100

    Adds scrollReveal('.scroll-reveal') to common.init();

--- web/app/themes/sage/assets/scripts/routes/common.js
+++ web/app/themes/sage/assets/scripts/routes/common.js
@@ -1,8 +1,11 @@
+import scrollReveal from '../components/scrollReveal/index';
+
 export default {
-  init() {
-    // JavaScript to be fired on all pages
-  },
-  finalize() {
-    // JavaScript to be fired on all pages, after page specific JS is fired
-  },
+	init() {
+		// JavaScript to be fired on all pages
+		scrollReveal('.scroll-reveal');
+	},
+	finalize() {
+		// JavaScript to be fired on all pages, after page specific JS is fired
+	},
 };
