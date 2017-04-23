a420468e68e2e89d7a6c791dad450222409f3542
Scott Cox <scottcox89@gmail.com>
2017-04-23 13:41:50 +0100

    Changes logic to bring in recent photos posts rather than the most recent images from photo posts.

--- web/app/themes/sage/templates/partials/recent-photos.blade.php
+++ web/app/themes/sage/templates/partials/recent-photos.blade.php
@@ -1,45 +1,29 @@
 <?php
 $args = [
 	'post_type'  => 'photos',
-	'post_count' => 2,
+	'post_count' => 3,
 ];
 
 $query = new WP_Query($args);
-
-$imagesToDisplay = [];
-
-while ($query->have_posts()) {
-	$query->the_post();
-	$images = get_post_galleries_images();
-	if (count($imagesToDisplay, COUNT_RECURSIVE) <= 8) {
-		foreach ($images as $imageList) {
-			foreach ($imageList as $image) {
-				$title = get_the_title();
-				if (!array_key_exists($title, $imagesToDisplay)) {
-					$imagesToDisplay[$title] = [];
-				}
-				array_unshift($imagesToDisplay[$title], $image);
-			}
-		}
-	}
-}
 ?>
 
 @if($query->have_posts())
 	<section class="recent-photos scroll-reveal">
 		<div class="container">
-			<h1 class="text-center">Recent Photos</h1>
 			<a href="/photos/">
-				<div class="flex-container">
-					@foreach($imagesToDisplay as $title => $gallery)
-						@foreach($gallery as $imgSrc)
-							@include('components.images.photo-tile', [
-								'imgSrc' => $imgSrc
-							])
-						@endforeach
-					@endforeach
-				</div>
+				<h1>Recent Photos</h1>
 			</a>
+			<div class="row">
+				@while($query->have_posts()) @php($query->the_post())
+				<div class="col-sm-4">
+					@include('components.images.photo-tile_text-overlay', [
+						'imgSrc' => get_the_post_thumbnail_url(),
+						'href' => get_the_permalink(),
+						'title' => get_the_title(),
+					])
+				</div>
+				@endwhile
+			</div>
 		</div>
 	</section>
 @endif
