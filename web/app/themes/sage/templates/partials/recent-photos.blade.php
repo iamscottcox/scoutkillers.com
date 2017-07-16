b515b44b69461a3b361343c72e13119ec0725792
Scott Cox <scottcox89@gmail.com>
2017-07-16 11:33:08 +0100

    Multiple Changes: * Adds .photo-floating-panel and .photo-floating-panel-reverse components and styles. * Integrates .photo-floating-panel components.

--- web/app/themes/sage/templates/partials/recent-photos.blade.php
+++ web/app/themes/sage/templates/partials/recent-photos.blade.php
@@ -3,6 +3,7 @@ $args = [
 	'post_type'  => 'photos',
 	'post_count' => 3,
 ];
+$index = 1;
 
 $query = new WP_Query($args);
 ?>
@@ -14,12 +15,21 @@ $query = new WP_Query($args);
 			@while($query->have_posts()) @php($query->the_post())
 			<div class="row">
 				<div class="col-12">
-					@include('components.images.photo-floating-panel', [
+					@if($index % 2 !== 0)
+						@include('components.images.photo-floating-panel', [
+							'imgSrc' => get_the_post_thumbnail_url(),
+							'href' => get_the_permalink(),
+							'title' => get_the_title(),
+						])
+					@else
+						@include('components.images.photo-floating-panel-reverse', [
 						'imgSrc' => get_the_post_thumbnail_url(),
-						'href' => get_the_permalink(),
-						'title' => get_the_title(),
-					])
+							'href' => get_the_permalink(),
+							'title' => get_the_title(),
+						])
+					@endif
 				</div>
+				@php($index++)
 			</div>
 			@endwhile
 		</div>
