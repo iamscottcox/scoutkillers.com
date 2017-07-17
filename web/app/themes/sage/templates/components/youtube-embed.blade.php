cb2db0863b62f158cebc7d8bb692e6850ebfb23b
Scott Cox <scottcox89@gmail.com>
2017-07-17 22:52:47 +0100

    Changes styling on .featured-video

--- web/app/themes/sage/templates/components/youtube-embed.blade.php
+++ web/app/themes/sage/templates/components/youtube-embed.blade.php
@@ -1,5 +1,19 @@
+<?php
+$today = time();
+
+$release_date = get_field('release_date');
+$release_date = substr($release_date,6,4)."-".substr($release_date,3,2)."-".substr($release_date,0,2);
+$release_date = strtotime($release_date);
+
+$out_now = $release_date - $today <= 0 ? true : false;
+?>
+
 <p class="h1 feature-title">{{ get_the_title() }}</p>
 <iframe src="https://www.youtube.com/embed/{{ get_field('embed_code') }}"
 		frameborder="0"
 		allowfullscreen></iframe>
-<p class="h4 text-center">Availble from: {{ get_field('release_date') }}</p>
+@if($out_now)
+	<p class="h1 text-center">OUT NOW</p>
+	@else
+	<p class="h1 text-center">{{ get_field('release_date') }}</p>
+@endif
